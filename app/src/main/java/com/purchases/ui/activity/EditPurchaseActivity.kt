80c7caaea65a19a20c8b79c345c9021f9690bf67
Andrey Plis <plisandrey91@gmail.com>
2017-11-21 08:52:23 +0300

    updated search activity

--- app/src/main/java/com/purchases/ui/activity/EditPurchaseActivity.kt
+++ app/src/main/java/com/purchases/ui/activity/EditPurchaseActivity.kt
@@ -9,6 +9,9 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.hannesdorfmann.mosby3.mvp.MvpActivity
 import com.purchases.R
+import com.purchases.mvp.model.Good
+import com.purchases.mvp.model.Measure
+import com.purchases.mvp.model.Purchase
 import com.purchases.mvp.model.Purchases
 import com.purchases.mvp.presenter.PurchasePresenter
 import com.purchases.mvp.view.PurchaseView
@@ -68,32 +71,28 @@ class EditPurchaseActivity : MvpActivity<PurchaseView, PurchasePresenter>(), Pur
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        var good = data!!.getStringArrayExtra("good")
-        /*realm.executeTransactionAsync { realm ->
+        val goodName = data!!.getStringExtra("goods")
+        val count = data!!.getFloatExtra("count",0f)
+        val measure = data!!.getStringExtra("measure")
+        realm.executeTransactionAsync { realm ->
 
-         var good = realm.where(Good::class.java).equalTo("name",good.name).findFirst()
-         if(good == null)
-         {
-             good = realm.createObject(Good::class.java, good?.name)
-         }
+            var good = realm.where(Good::class.java).equalTo("name", goodName).findFirst()
+            if (good == null) {
+                good = realm.createObject(Good::class.java, goodName)
+            }
+            var purchase = realm.createObject(Purchase::class.java, System.currentTimeMillis())
+            purchase.good = good
+            purchase.count = count
+            purchase.measure = realm.where(Measure::class.java).equalTo("name", measure).findFirst()
 
-     }*/
+            var purchases = realm.where(Purchases::class.java).equalTo("id", idPurchases).findFirst()
+            purchases!!.purchase.add(purchase)
+        }
     }
 
     private fun onClickAdd() {
         val intent = Intent(this, SearchActivity::class.java)
-        startActivityForResult(intent,1)
-        /*realm.executeTransactionAsync { realm ->
-            var good = realm.createObject(Goods::class.java, "gfhfhfg")
-            var purchase = realm.createObject(Purchase::class.java, System.currentTimeMillis())
-            purchase.count = 1.0f
-            purchase.good = good
-
-            realm.where(Purchases::class.java).equalTo("id", idPurchases)
-                    .findFirst()!!
-                    .purchase.add(purchase)
-        }*/
-
+        startActivityForResult(intent, 1)
     }
 
     private fun onClickAddFavorite() {
